db64464b287934477fde888d3a08c7ce2564eb1a
Joseph Vint <josephvint@me.com>
2017-10-26 23:49:29 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow([0, 1, 2, 3, 4, 5, 6])
+def display_rainbow(0, 1, 2, 3, 4, 5, 6)
   puts "R:#{0}, O:#{1}, Y:#{2}, G:#{3}, B:#{4}, I:#{5}, V:#{6}"
 end
